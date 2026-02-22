5c705005903b717c6907513c5ccb9545c4f5afd0
Eric Fortmeyer <e.fortmeyer@gmail.com>
2026-02-22 10:32:16 -0700

    fix: use int for id type, change bath field to baths
    Signed-off-by: Eric Fortmeyer <e.fortmeyer@gmail.com>

--- src/PropertyImage.php
+++ src/PropertyImage.php
@@ -4,6 +4,7 @@ namespace ClutchPropertyMgmt\BusinessDomain;
 
 final class PropertyImage
 {
+    public int $id;
     public int $property_id;
     public string $url;
     public string $mime_type;
